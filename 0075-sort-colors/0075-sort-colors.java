72ce30a726438bc7c1cbf2fdeed4de06f29401a6
Chetan Khera <chetan_be23al023@srmcem.ac.in>
2026-06-23 13:50:18 +0530

    Time: 0 ms (100%), Space: 43.5 MB (41.85%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,28 @@
+class Solution {
+    private void swap(int[] nums, int i, int j) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+// nd
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                // Swap mid and low, then move both pointers forward
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                // 1 is in the correct middle position, just move mid forward
+                mid++;
+                      } else if (nums[mid] == 2) {
+                // Swap mid and high, move high backward
+                // Do NOT move mid forward yet, because the swapped element needs evaluation
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }}
